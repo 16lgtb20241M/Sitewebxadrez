5186ef20daf24fb385d95ededff60c2dec2e742b
16lgtb20241M <brasil.leticia@escola.pr.gov.br>
2025-05-25 18:46:01 -0300

    trabalho finalizado

---
+++ perguntas.js
@@ -0,0 +1,71 @@
+criaCartao(
+    'Xadrez Aberturas',
+    'Abertura Italiana',
+    'Movimentos iniciais: 1. e4 e5 2. Nf3 Nc6.'
+)
+
+criaCartao(
+    'Xadrez Aberturas',
+    'Abertura Ruy Lopez',
+    'Movimentos iniciais: 1. e4 e5 2. Nf3 Nc6 3. Bb5.'
+)
+
+criaCartao(
+    'Xadrez Aberturas',
+    'Abertura Sistema London',
+    'Movimentos iniciais: 1. d4 d5 2. Nf3 Nf6 3. Bf4 e6.'
+)
+
+criaCartao(
+    'Xadrez Aberturas',
+    'Abertura Siciliana',
+    'Movimentos iniciais: 1. e4 c5.'
+)
+
+criaCartao(
+    'Xadrez Defesa',
+    'Defesa Francesa',
+    'Movimentos iniciais: 1. e4 e6 2. d4 d5.'
+)
+
+criaCartao(
+    'Xadrez Defesa',
+    'Defesa Caro-Kann',
+    'Movimentos iniciais: 1. e4 c6 2. d4 d5.'
+)
+
+criaCartao(
+    'Xadrez Aberturas',
+    'Abertura Inglesa',
+    'Movimentos iniciais: 1. c4.'
+)
+
+criaCartao(
+    'Xadrez Aberturas',
+    'Abertura Réti',
+    'Movimentos iniciais: 1. Nf3 d5 2. c4.'
+)
+
+criaCartao(
+    'Xadrez Aberturas',
+    'Gambito da Dama',
+    'Movimentos iniciais: 1. d4 d5 2. c4.'
+)
+
+criaCartao(
+    'Xadrez Defesa',
+    'Defesa Índia do Rei',
+    'Movimentos iniciais: 1. d4 Nf6 2. c4 g6 3. Nc3 Bg7.'
+)
+
+criaCartao(
+    'Xadrez Defesa',
+    'Defesa Grunfeld',
+    'Movimentos iniciais: 1. d4 Nf6 2. c4 g6 3. Nc3 d5.'
+)
+
+criaCartao(
+    'Xadrez Defesa',
+    'Defesa Nimzo-Índia',
+    'Movimentos iniciais: 1. d4 Nf6 2. c4 e6 3. Nc3 Bb4.'
+)
